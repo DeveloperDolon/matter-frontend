5cfa8984e894ce498d3330e57224dc7569350545
Dolon Roy <dolonr718@gmail.com>
2023-11-24 02:08:45 +0600

    navbar added commit

---
+++ src/Components/NabBar/NavBar.jsx
@@ -0,0 +1,197 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import { NavLink } from 'react-router-dom';
+import logo from "../../assets/logo.png";
+
+
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function ResponsiveAppBar() {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    const navBar = (color) => {
+        return <>
+        <Button
+            className='md:w-auto w-full text-left'
+            onClick={handleCloseNavMenu}
+            sx={{ my: 2, color: color, display: 'block' }}
+        >
+            <NavLink to={"/"} 
+            className={({ isActive, isPending }) =>
+            isPending ? "pending w-full md:text-base text-xs font-bold block" : isActive ? "border-b-2 border-blue-100 w-full md:text-base text-xs font-bold block" : "w-full md:text-base text-xs font-bold block"
+            }
+            >
+                Home
+            </NavLink>
+        </Button>
+        <Button
+            className='md:w-auto w-full text-left'
+            onClick={handleCloseNavMenu}
+            sx={{ my: 2, color: color, display: 'block' }}
+        >
+            <NavLink className={"w-full md:text-base text-xs font-bold block"}>
+                All Properties
+            </NavLink>
+        </Button>
+        <Button
+            className='md:w-auto w-full text-left'
+            onClick={handleCloseNavMenu}
+            sx={{ my: 2, color: color, display: 'block' }}
+        >
+            <NavLink className={"w-full md:text-base text-xs font-bold block"}>
+                Dashboard
+            </NavLink>
+        </Button>
+        <Button
+            className='md:w-auto w-full text-left'
+            onClick={handleCloseNavMenu}
+            sx={{ my: 2, color: color, display: 'block' }}
+        >
+            <NavLink className={"w-full md:text-base text-xs font-bold block"}>
+                Login
+            </NavLink>
+        </Button>
+    </>
+    } 
+
+    return (
+        <AppBar position="static" sx={{ background: "#121414" }} className='w-full'>
+            <Container maxWidth="xl" className='bg-[#121414]'>
+                <Toolbar disableGutters>
+                    <img src={logo} className='md:w-16 md:block hidden w-5 mr-2 py-3' />
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        sx={{
+                            mr: 5,
+                            fontSize: "25px",
+                            fontFamily: "Montserrat",
+                            display: { xs: 'none', md: 'flex' },
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >   
+                        MATTER
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {navBar("black")}
+                        </Menu>
+                    </Box>
+                    <img src={logo} className='md:hidden block w-10 mr-2 py-3' />
+                    <Typography
+                        variant="h5"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'flex', md: 'none' },
+                            flexGrow: 1,
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        MATTER
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex', color: "black"} }}>
+                        {navBar("white")}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+export default ResponsiveAppBar;
--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import ResponsiveAppBar from "../Components/NabBar/NavBar";
 
-
+// https://player.vimeo.com/video/766143045?autoplay=1&loop=1&background=1&hd=1&controls=0
 const MainLayout = () => {
     return (
         <div>
-            hello world from main layout
+            <ResponsiveAppBar></ResponsiveAppBar>
+            
             <Outlet></Outlet>
         </div>
     );
